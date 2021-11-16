1f0f92b5d05de7658c7726d065c9d7247d35d8f4
Ivan Caldeira Sanches <sanches.ic.89@gmail.com>
2021-11-16 11:11:42 -0300

    📦 storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,6 @@
+const fs = require('fs')
+const path = require('path')
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
@@ -5,4 +8,25 @@ module.exports = {
     '@storybook/addon-essentials',
     '@storybook/addon-a11y',
   ],
+
+  webpackFinal: async config => {
+    config.resolve.modules = [path.resolve(__dirname, '..'), 'node_modules']
+
+    const paths = fs
+      .readdirSync(path.resolve(__dirname, '../src'))
+      .filter(f =>
+        fs.statSync(path.join(__dirname, `../src/${f}`)).isDirectory(),
+      )
+      .reduce((acc, dir) => {
+        acc[`@/${dir}`] = path.resolve(__dirname, `../src/${dir}`)
+        return acc
+      }, {})
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      ...paths,
+    }
+
+    return config
+  },
 }
