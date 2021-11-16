bb829a6d5f1756039ee40c2287b3371af2abd19c
Ivan Caldeira Sanches <sanches.ic.89@gmail.com>
2021-11-16 10:14:28 -0300

    📦 storybook config

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,20 @@
+import React from 'react'
+
+import {addDecorator} from '@storybook/react'
+import {ThemeProvider} from 'styled-components'
+
+import {GlobalStyle} from '../src/styles/GlobalStyle'
+import {theme} from '../src/styles/theme'
+
+const StylesDecorator = storyFn => (
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    {storyFn()}
+  </ThemeProvider>
+)
+
+addDecorator(StylesDecorator)
+
 export const parameters = {
   actions: {argTypesRegex: '^on[A-Z].*'},
   controls: {
