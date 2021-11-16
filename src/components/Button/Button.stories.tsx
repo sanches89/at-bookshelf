5cda6976a1657ab04dc40fa606f43861ceea0a5c
Ivan Caldeira Sanches <sanches.ic.89@gmail.com>
2021-11-16 10:15:07 -0300

    🚀 button

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -20,8 +20,3 @@ export const Primary = Template.bind({})
 Primary.args = {
   children: 'Primary',
 }
-
-export const Secondary = Template.bind({})
-Secondary.args = {
-  children: 'Secondary',
-}
--- src/components/Button/Button.styles.ts
+++ src/components/Button/Button.styles.ts
@@ -1,3 +1,15 @@
-import styled from 'styled-components'
+import styled, {css} from 'styled-components'
 
-export const Container = styled.button``
+export const Container = styled.button`
+  ${({theme}) => css`
+    padding: 14px 22px;
+
+    border: none;
+    border-radius: 6px;
+
+    background-color: ${theme.color.primary};
+
+    color: ${theme.color.primaryText};
+    font-weight: 700;
+  `}
+`
